05e4897970d5c6589a78e2eaed302445e0a3afd8
h-adji <h-adji_tech@proton.me>
2024-08-16 08:10:10 +0200

    add lsp

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,15 @@
-
 -- indentation and tabs
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4 
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 vim.g.mapleader = " "
+vim.opt.clipboard:append("unnamedplus")
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
