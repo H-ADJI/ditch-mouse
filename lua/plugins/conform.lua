45ff8dab0745845ee03fa53d98272caa33e4c650
h-adji <h-adji_tech@proton.me>
2024-08-17 14:39:07 +0200

    add formatting shortcuts

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,26 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                     lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                -- python = { "isort", "black" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                -- rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                -- javascript = { "prettierd", "prettier", stop_after_first = true },
-            },
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-        })
-    end
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<leader>lf", ":ConformInfo<CR>", opts)
+		vim.keymap.set("n", "<leader>fa", conform.format, opts)
+		vim.keymap.set({ "v", "x" }, "<leader>cf", conform.format, opts)
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				-- rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
+	end,
 }
