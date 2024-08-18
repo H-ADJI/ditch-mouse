6d8dca5948e22a5ff3edd393d4b89f31d870285f
h-adji <h-adji_tech@proton.me>
2024-08-18 22:14:08 +0200

    moved to lazynvim distro with some plugins in example.lua

--- lua/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- highling / search keymap
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>",opts)
-vim.keymap.set("n", "<leader>e", ":Neotree focus filesystem float toggle<CR>", opts)
-vim.keymap.set("n", "<leader>ll", ":Lazy<CR>", opts)
--- lua/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local options = { theme = "codedark" }
-		require("lualine").setup({ options = options })
-	end,
-}
