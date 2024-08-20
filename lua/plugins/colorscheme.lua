e5c17db901efa550aa938142184ef299b8688171
khalil hadji <khalil.hadji@dataimpact.io>
2024-08-20 21:34:26 +0200

    add colorsschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "tomasiser/vim-code-dark" },
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- }
+}
