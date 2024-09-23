020112aff063334708955fcc3c86e3ac1a4cc78a
khalil hadji <khalil.hadji@dataimpact.io>
2024-09-23 10:56:34 +0200

    add smart frequency telescope extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
+      { "<leader>fr", false },
       {
         "<leader>fu",
         "<cmd> Telescope find_files find_command=rg,--ignore,--hidden,--files,-u<cr>",
@@ -39,5 +40,14 @@ return {
     config = function()
       require("telescope").load_extension("file_browser")
     end,
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require("telescope").load_extension("frecency")
+    end,
+    keys = {
+      { "<leader>fr", "<cmd>Telescope frecency workspace=CWD<cr>", desc = "[F]ind [R]ecent buffer" },
+    },
   },
 }
